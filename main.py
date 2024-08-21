6b7cb7b0c7abe31b41923bb973b7a623069041ba
Kha Dang <dminhkha@gmail.com>
2024-08-21 16:56:26 +0700

    Image header and changed UI

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from tkinter import *
 from tkinter import scrolledtext
 import pyperclip
 
+
+font_sd = ("Helvetica", "14", "bold")
 def copy_text():
     text_to_copy = text_area.get("1.0", tk.END)
     pyperclip.copy(text_to_copy)
@@ -16,20 +18,23 @@ def generate_code():
     l = float(l_entry.get())
     t = float(t_entry.get())
     o = float(o_entry.get())
-
-    match t:
-        case t if 0 < t <=1:
-            fcut = 7650
-        case t if 1 < t <=2:
-            fcut = 5560
-        case t if 2 < t <= 3:
-            fcut = 3960
-        case t if 3 < t <= 4:
-            fcut = 2800
-        case t if 4 < t <= 5:
-            fcut = 1980
-        case _:
-            fcut = 1430
+    fc = float(fcut_entry.get())
+    if fc == 0:
+        match t:
+            case t if 0 < t <=1:
+                fcut = 7650
+            case t if 1 < t <=2:
+                fcut = 5560
+            case t if 2 < t <= 3:
+                fcut = 3960
+            case t if 3 < t <= 4:
+                fcut = 2800
+            case t if 4 < t <= 5:
+                fcut = 1980
+            case _:
+                fcut = 1430
+    else:
+        fcut = fc
 
     g_code_entry = "G90 G17 G21 \nG54\n"
 
@@ -48,43 +53,82 @@ def generate_code():
 BACKGROUND_COLOR = "#B1DDC6"
 window = Tk()
 window.title("Simple cut G-Code generator")
+window.geometry('1000x800')
+#window.columnconfigure((0,1,2), weight=1)
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-canvas = Canvas(width=600, height=100)
-canvas.grid(row=0, column=1)
+
+img_tk = tk.PhotoImage(file="imgs/header_image2.png")
+canvas = Canvas(width=780, height=80)
+canvas.create_image(400,100,image=img_tk, anchor=S)
+canvas.grid(row=0, column=0, columnspan=3)
 
 #Labels
-n_label = Label(text='n', font=12)
+n_label = Label(text='Số lần cắt n', font=font_sd, bg=BACKGROUND_COLOR)
 n_label.grid(row=1, column=0, sticky="E", pady=5)
-l_label = Label(text="interval length(mm)",font=12)
+
+l_label = Label(text="Chiều dài cắt L(mm)",font=font_sd, bg=BACKGROUND_COLOR)
 l_label.grid(row=2, column=0, sticky="E", pady=5)
-t_label = Label(text="thickness(mm)",font=12)
+
+t_label = Label(text="Độ dày ống t(mm)",font=font_sd, bg=BACKGROUND_COLOR)
 t_label.grid(row=3,  column=0, sticky="E", pady=5)
-o_label = Label(text="offset(mm)",font=12)
+
+o_label = Label(text="Bù chều dài o(mm)",font=font_sd, bg=BACKGROUND_COLOR)
 o_label.grid(row=4,  column=0, sticky="E", pady=5)
 
+fcut_label = Label(text="Tốc độ cắt", font=("Helvetica", "12", "italic"), bg=BACKGROUND_COLOR,fg="#0D7C66")
+fcut_label.grid(row=5, column=0, sticky="ES", pady=1)
+
+# Infor ref
+text_ref = ("Bảng tham chiếu tốc độ cắt\n"
+            "   t(mm)            F\n"
+            "   1               7650\n"
+            "   2               5560\n"
+            "   3               3960\n"
+            "   4               2800\n"
+            "   5               1980\n"
+            "   6               1430\n"
+            "Phần mềm đã tự động tính toán tốc độ \n"
+            "dựa trên thông số t đã input. \n"
+            "Tuy nhiên bạn có thể hiệu chỉnh \n"
+            "tốc độ cắt trong field dưới đây.\n"
+            "Lưu ý nhập 0 nếu muốn phần mềm tự tính toán.\n"
+            )
+ref_label = Label(text=text_ref, font=("Helvetica", "11", "italic"), bg=BACKGROUND_COLOR,fg="#0D7C66" )
+ref_label.grid(row=5, column=0, columnspan=2, sticky="NEW", pady=10)
+
 # Entries
-n_entry = Entry(width=35, font=12)
+n_entry = Entry(width=15, font=12)
 n_entry.insert(0,1)
 n_entry.focus()
 n_entry.grid(row=1, column=1, sticky='W', pady=5)
-l_entry = Entry(width=35,font=12)
+
+l_entry = Entry(width=15,font=12)
 l_entry.insert(0,10)
 l_entry.grid(row=2, column=1,sticky='W', pady=5)
-t_entry = Entry(width=35,font=12)
+
+t_entry = Entry(width=15,font=12)
 t_entry.insert(0,1)
 t_entry.grid(row=3, column=1,sticky='W', pady=5)
-o_entry = Entry(width=35,font=12)
+
+o_entry = Entry(width=15,font=12)
 o_entry.insert(0,2)
 o_entry.grid(row=4, column=1,sticky='W', pady=5)
 
+fcut_entry =  Entry(width=10, font=8)
+fcut_entry.insert(0,0)
+fcut_entry.grid(row=5, column=1, sticky='SW', pady=1)
+
 #Buttons
-generate_code_but = Button(text="Generate G-Code", width=36, command=generate_code)
-generate_code_but.grid(row=1, column=1, sticky="E")
-clear_code_but = Button(text="Clear", width=36, command=clear_code)
-clear_code_but.grid(row=2, column=1, sticky="E")
-copy_but = Button(text="Copy", width=20, command=copy_text)
-copy_but.grid(row=6, column=1, sticky="SE", pady=10)
+generate_code_but = Button(text="Tạo G-Code",command=generate_code)
+generate_code_but.grid( row=6,column=0,columnspan=2, sticky="WE")
+
+clear_code_but = Button(text="Xóa", width=20, command=clear_code)
+clear_code_but.grid(row=6, column=2, sticky="W", padx=10)
+
+copy_but = Button(text="Sao chép", width=20, command=copy_text)
+copy_but.grid(row=6, column=2, sticky="E", pady=10, padx=5)
+
 #Output
-text_area = scrolledtext.ScrolledText(window, wrap=tk.WORD, width= 73, height= 30, font=('Times New Roman', 12))
-text_area.grid(row=5,column=1, columnspan=2)
+text_area = scrolledtext.ScrolledText(window, wrap=tk.WORD, width= 45, height= 28, font=("Helvetica", "12"))
+text_area.grid(row=1,column=2, rowspan=5, sticky="NS",pady=5, padx=5)
 window.mainloop()
